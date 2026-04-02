1f4f4726cd121a559607b69daeff27f916976a14
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-02 15:44:46 +0700

    membuat kerangka fungsi mergeSort dengan parameter low dan high

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -22,5 +22,26 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+
+    cout << "\n-------------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "-------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
-    
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
+    int mid = (low + high) / 2; // step 2
+
+   
